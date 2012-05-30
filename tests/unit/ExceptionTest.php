ed64ac10ba7d98373874f699386a2ba93c436251
Brandon Lamb <brandon@brandonlamb.com>
2012-05-29 18:17:23 -0700

    added test for exception classes

---
+++ tests/unit/ExceptionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @small
+ */
+class ExceptionTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers Eve\Exception
+	 */
+	public function testException()
+	{
+		$this->assertTrue(true);
+	}
+}
