c102d2a733983587f57158ed8e39d0c351c4591f
Brandon Lamb <brandon@brandonlamb.com>
2013-02-23 23:16:33 -0800

    Cleanup of Pdo class

--- src/Eve/Database/Pdo.php
+++ src/Eve/Database/Pdo.php
@@ -3,185 +3,120 @@ namespace Eve\Database;
 
 class Pdo extends \PDO
 {
-    /**
-     * Connection settings
-     *
-     * @var string
-     */
-    protected $type;
-    protected $dsn;
-    protected $username;
-    protected $password;
+	/**
+	 * Constructor
+	 *
+	 * @param  string $type
+	 * @param  string $dsn
+	 * @param  string $username
+	 * @param  string $password
+	 * @param  bool   $noerrors
+	 * @return void
+	 */
+	public function __construct($dsn, $username, $password, array $options = array())
+	{
+		try {
+			parent::__construct($dsn, $username, $password);
+			$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		} catch (\PDOException $e) {
+			throw new \RuntimeException($e->getMessage());
+		}
+	}
 
-    /**
-     * Constructor
-     *
-     * @param  string $type
-     * @param  string $dsn
-     * @param  string $username
-     * @param  string $password
-     * @param  bool   $noerrors
-     * @return void
-     */
-    public function __construct($dsn, $username, $password)
-    {
-        // Set error setting
-        $this->dsn		= $dsn;
-        $this->username	= $username;
-        $this->password	= $password;
+	/**
+	 * Run prepared statement, return multiple rowsets
+	 *
+	 * @param  string $query
+	 * @param  array  $parameters
+	 * @return array
+	 */
+	public function fetchAll($query, array $parameters = array())
+	{
+		$stmt = $this->prepareExecute($query, $parameters);
 
-        // Verify dsn type
-        $type = substr($dsn, 0, strpos($dsn, ':'));
-        switch ($type) {
-            case 'mysql':
-                break;
-            case 'mssql':
-                break;
-            case 'dblib':
-                break;
-            case 'sqlsrv':
-                break;
-            case 'ibm':
-                break;
-            default:
-                $type = 'mysql';
-        }
-        $this->type = $type;
+		// Fetch rows as associative array
+		$rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
-        $this->connect();
-    }
+		// Close cursor and delete $stmt variable
+		$stmt->closeCursor();
+		unset($stmt);
 
-    /**
-     * Create database connection
-     *
-     * @throws Exception
-     * @return void
-     */
-    protected function connect()
-    {
-        try {
-            parent::__construct($this->dsn, $this->username, $this->password);
+		// Return rows array
+		return $rows;
+	}
 
-            // Added to mysql connections to prevent weird error
-            switch ($this->type) {
-                case 'mysql':
-                    \PDO::setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-                    break;
-                case 'ibm':
-                    \PDO::setAttribute(\PDO::ATTR_PERSISTENT, true);
-                    \PDO::setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                    break;
-                default:
-            }
-        } catch (PDOException $e) {
-            throw new \RuntimeException($e->getMessage());
-        }
-    }
+	/**
+	 * Run prepared statement, return single rowset
+	 *
+	 * @param  string     $query
+	 * @param  array      $parameters
+	 * @return array|bool
+	 */
+	public function fetchOne($query, array $parameters = array())
+	{
+		$stmt = $this->prepareExecute($query, $parameters);
 
-    /**
-     * Getter for database type
-     *
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
+		// Fetch rows as associative array
+		$row = $stmt->fetch(\PDO::FETCH_ASSOC);
+		if (!is_array($row)) {
+			$row = false;
+		}
 
-    /**
-     * Run prepared statement, return multiple rowsets
-     *
-     * @param  string $query
-     * @param  array  $parameters
-     * @return array
-     */
-    public function fetchAll($query, array $parameters = array())
-    {
-        $stmt = $this->prepareExecute($query, $parameters);
+		// Close cursor and delete $stmt variable
+		$stmt->closeCursor();
+		unset($stmt);
 
-        // Fetch rows as associative array
-        $rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		// Return rows array
+		return $row;
+	}
 
-        // Close cursor and delete $stmt variable
-        $stmt->closeCursor();
-        unset($stmt);
+	/**
+	 * Fetch a single result column
+	 *
+	 * @param  string $query
+	 * @param  array  $parameters
+	 * @param  int    $column
+	 * @return mixed
+	 */
+	public function fetchColumn($query, array $parameters = array(), $column = 0)
+	{
+		$column = abs((int) $column);
 
-        // Return rows array
-        return $rows;
-    }
+		$stmt = $this->prepareExecute($query, $parameters);
+		$fetchedColumn = $stmt->fetchColumn($column);
 
-    /**
-     * Run prepared statement, return single rowset
-     *
-     * @param  string     $query
-     * @param  array      $parameters
-     * @return array|bool
-     */
-    public function fetchOne($query, array $parameters = array())
-    {
-        $stmt = $this->prepareExecute($query, $parameters);
+		$stmt->closeCursor();
+		unset($stmt);
 
-        // Fetch rows as associative array
-        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
-        if (!is_array($row)) {
-            $row = false;
-        }
+		return $fetchedColumn;
+	}
 
-        // Close cursor and delete $stmt variable
-        $stmt->closeCursor();
-        unset($stmt);
+	/**
+	 * Execute an INSERT/UPDATE/DELETE statement
+	 *
+	 * @param  string $query
+	 * @param  array  $parameters
+	 * @return int
+	 */
+	public function modify($query, array $parameters = array())
+	{
+		$stmt = $this->prepareExecute($query, $parameters);
 
-        // Return rows array
-        return $row;
-    }
+		return $stmt->rowCount();
+	}
 
-    /**
-     * Fetch a single result column
-     *
-     * @param  string $query
-     * @param  array  $parameters
-     * @param  int    $column
-     * @return mixed
-     */
-    public function fetchColumn($query, array $parameters = array(), $column = 0)
-    {
-        $column = abs((int) $column);
+	/**
+	 * Prepare and execute prepared statement
+	 *
+	 * @param  string         $query
+	 * @param  array          $parameters
+	 * @return PDO::Statement
+	 */
+	protected function prepareExecute($query, $parameters = array())
+	{
+		$stmt = $this->prepare($query);
+		$stmt->execute($parameters);
 
-        $stmt = $this->prepareExecute($query, $parameters);
-        $fetchedColumn = $stmt->fetchColumn($column);
-
-        $stmt->closeCursor();
-        unset($stmt);
-
-        return $fetchedColumn;
-    }
-
-    /**
-     * Execute an INSERT/UPDATE/DELETE statement
-     *
-     * @param  string $query
-     * @param  array  $parameters
-     * @return int
-     */
-    public function modify($query, array $parameters = array())
-    {
-        $stmt = $this->prepareExecute($query, $parameters);
-
-        return $stmt->rowCount();
-    }
-
-    /**
-     * Prepare and execute prepared statement
-     *
-     * @param  string         $query
-     * @param  array          $parameters
-     * @return PDO::Statement
-     */
-    protected function prepareExecute($query, $parameters = array())
-    {
-        $stmt = $this->prepare($query);
-        $stmt->execute($parameters);
-
-        return $stmt;
-    }
+		return $stmt;
+	}
 }
