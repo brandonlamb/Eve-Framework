ffb58e87c40e794e69a0972d7217405e119a4a2e
Brandon Lamb <brandon@brandonlamb.com>
2013-05-04 15:24:58 -0700

    Move Log to Logger/File

--- src/Eve/Logger/File.php
+++ src/Eve/Logger/File.php
@@ -1,186 +1,128 @@
 <?php
+/**
+ * Simple logger class based on a similer class created by
+ * Darko Bunic (http://www.redips.net/php/write-to-log-file/)
+ * Does simple logging to a specified file. See https://bitbucket.org/huntlyc/simple-php-logger for more details.
+ *
+ * @package default
+ * @author Huntly Cameron <huntly.cameron@gmail.com>
+ */
 namespace Eve\Logger;
 
-class Logger
+class Log
 {
-    /**
-     * @var int Log level
-     */
-    protected $level = 0;
+	/**
+	 * @var string, the log file to write to
+	 */
+	protected $file;
 
-    /**
-     * @var array Log levels
-     */
-    protected $levels = array(
-        0 => 'FATAL',
-        1 => 'ERROR',
-        2 => 'WARN',
-        3 => 'INFO',
-        4 => 'DEBUG'
-   );
+	/**
+	 * Constructor
+	 * @param string $logfile, - [optional] Absolute file name/path. Defaults to ubuntu apache log.
+	 * @return void
+	 */
+	public function __construct($file = null)
+	{
+		$this->file = $file;
 
-    /**
-     * @var string Absolute path to log directory with trailing slash
-     */
-    protected $directory;
+		// If passed a filename, make sure it is writable
+		if (null !== $file && !is_writable($file)) {
+			throw new \RuntimeException("LOGGER ERROR: Can't write to log", 1);
+		}
+	}
 
-    /**
-     * Constructor
-     *
-     * @param string $directory Absolute or relative path to log directory
-     * @param int    $level     The maximum log level reported by this Logger
-     */
-    public function __construct($directory, $level = 4)
-    {
-        $this->setDirectory($directory);
-        $this->setLevel($level);
-    }
+	/**
+	 * Log Debug
+	 * @param string $tag, Log Tag
+	 * @param string $message, message to log
+	 * @return Log
+	 */
+	public function debug($tag, $message)
+	{
+		$this->write('DEBUG', $tag, $message);
+		return $this;
+	}
 
-    /**
-     * Set log directory
-     *
-     * @param  string $directory Absolute or relative path to log directory
-     * @return void
-     */
-    public function setDirectory($directory)
-    {
-        $this->directory = ($directory) ? realpath(rtrim($directory, '/') . '/') : false;
-    }
+	/**
+	 * Log Error
+	 * @param string $tag, Log Tag
+	 * @param string $message, message to log
+	 * @return Log
+	 */
+	public function error($tag, $message)
+	{
+		$this->write('ERROR', $tag, $message);
+		return $this;
+	}
 
-    /**
-     * Get log directory
-     *
-     * @return string|false Absolute path to log directory with trailing slash
-     */
-    public function getDirectory()
-    {
-        return (string) $this->directory;
-    }
+	/**
+	 * Log Warning
+	 * @param string $tag, Log Tag
+	 * @param string $message, message to log
+	 * @return Log
+	 */
+	public function warn($tag, $message)
+	{
+		$this->write('WARNING', $tag, $message);
+		return $this;
+	}
 
-    /**
-     * Set log level
-     *
-     * @param int The maximum log level reported by this Logger
-     * @return void
-     * @throws InvalidArgumentException If level specified is not 0, 1, 2, 3, 4
-     */
-    public function setLevel($level)
-    {
-        $level = (int) $level;
-        if ($level >= 0 && $level <= 4) {
-            $this->level = $level;
-        } else {
-            throw new \InvalidArgumentException('Invalid Log Level. Must be one of: 0, 1, 2, 3, 4.');
-        }
-    }
+	/**
+	 * Log Info
+	 * @param string $tag, Log Tag
+	 * @param string $message, message to log
+	 * @return Log
+	 */
+	public function info($tag, $message)
+	{
+		$this->write('INFO', $tag, $message);
+		return $this;
+	}
 
-    /**
-     * Get log level
-     *
-     * @return int
-     */
-    public function getLevel()
-    {
-        return (int) $this->level;
-    }
+	/**
+	 * Common log method with optional level
+	 * @param string $tag
+	 * @param string $message
+	 * @param string $level
+	 * @return Log
+	 */
+	public function log($tag, $message, $level = 'info')
+	{
+		switch ($level) {
+			case 'debug':
+				$this->debug($tag, $message);
+				break;
+			case 'info':
+				$this->info($tag, $message);
+				break;
+			case 'warn':
+				$this->warn($tag, $message);
+				break;
+			case 'error':
+				$this->error($tag, $message);
+				break;
+			default:
+				$this->info($tag, $message);
+		}
 
-    /**
-     * Log debug data
-     *
-     * @param  mixed $data
-     * @return void
-     */
-    public function debug($data)
-    {
-        $this->log($data, 4);
-    }
+		return $this;
+	}
 
-    /**
-     * Log info data
-     *
-     * @param  mixed $data
-     * @return void
-     */
-    public function info($data)
-    {
-        $this->log($data, 3);
-    }
+	/**
+	 * Writes out timestamped message to the log file as defined by the $file class variable.
+	 * @param string status - "INFO"/"DEBUG"/"ERROR" e.t.c.
+	 * @param string tag - "Small tag to help find log entries"
+	 * @param string message - The message you want to output.
+	 * @return void
+	 */
+	protected function write($status, $tag, $message)
+	{
+		$message = date('[Y-m-d H:i:s]') . ": [$tag][$status] - $message" . \PHP_EOL;
 
-    /**
-     * Log warn data
-     *
-     * @param  mixed $data
-     * @return void
-     */
-    public function warn($data)
-    {
-        $this->log($data, 2);
-    }
-
-    /**
-     * Log error data
-     *
-     * @param  mixed $data
-     * @return void
-     */
-    public function error($data)
-    {
-        $this->log($data, 1);
-    }
-
-    /**
-     * Log fatal data
-     *
-     * @param  mixed $data
-     * @return void
-     */
-    public function fatal($data)
-    {
-        $this->log($data, 0);
-    }
-
-    /**
-     * Get absolute path to current daily log file
-     *
-     * @return string
-     */
-    public function getFile()
-    {
-        return $this->getDirectory() . strftime('%Y-%m-%d') . '.log';
-    }
-
-    /**
-     * Log data to file
-     *
-     * @param  mixed            $data
-     * @param  int              $level
-     * @return void
-     * @throws RuntimeException If log directory not found or not writable
-     */
-    protected function log($data, $level)
-    {
-        $dir = $this->getDirectory();
-        if ($dir == false || !is_dir($dir)) {
-            throw new \RuntimeException("Log directory '$dir' invalid.");
-        }
-
-        if (!is_writable($dir)) {
-            throw new \RuntimeException("Log directory '$dir' not writable.");
-        }
-
-        if ($level <= $this->getLevel()) {
-            $this->write(sprintf("[%s] %s - %s\r\n", $this->levels[$level], date('c'), (string) $data));
-        }
-    }
-
-    /**
-     * Persist data to log
-     *
-     * @param string Log message
-     * @return void
-     */
-    protected function write($data)
-    {
-        @file_put_contents($this->getFile(), $data, \FILE_APPEND | \LOCK_EX);
-    }
+		if (null === $this->file) {
+			error_log($message, 0);
+		} else {
+	        @file_put_contents($this->file, $message, \FILE_APPEND | \LOCK_EX);
+		}
+	}
 }
